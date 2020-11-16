42e33a41d674e846c6952341596825bf965d2c2a
Dennis Briner <dennis.briner@lightray.io>
2020-11-16 15:59:16 +0100

    remove sample code

--- Android/app/src/main/java/no/nordicsemi/android/nrftoolbox/hygrometer/callback/HeaterStateCharacteristicCallback.java
+++
@@ -1,8 +0,0 @@
-package no.nordicsemi.android.nrftoolbox.hygrometer.callback;
-
-import android.bluetooth.BluetoothDevice;
-import androidx.annotation.NonNull;
-
-public interface HeaterStateCharacteristicCallback {
-	void onHeaterStateValueReceived(@NonNull final BluetoothDevice device, boolean value);
-}
--- Android/app/src/main/java/no/nordicsemi/android/nrftoolbox/hygrometer/callback/HumidityMeasurementCharacteristicCallback.java
+++
@@ -1,8 +0,0 @@
-package no.nordicsemi.android.nrftoolbox.hygrometer.callback;
-
-import android.bluetooth.BluetoothDevice;
-import androidx.annotation.NonNull;
-
-public interface HumidityMeasurementCharacteristicCallback {
-	void onHumidityMeasurementValueReceived(@NonNull final BluetoothDevice device, double value);
-}
