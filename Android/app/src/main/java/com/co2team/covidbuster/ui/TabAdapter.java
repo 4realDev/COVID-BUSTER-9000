968c05fc3bb0f62057cc3a03eb6aa97483dac683
Vladimir_Brazhnik <v.brazhnik@web.de>
2020-11-25 19:44:39 +0100

    Migrate TabAdapter to androidX

--- Android/app/src/main/java/com/co2team/covidbuster/ui/TabAdapter.java
+++ Android/app/src/main/java/com/co2team/covidbuster/ui/TabAdapter.java
@@ -1,10 +1,11 @@
 package com.co2team.covidbuster.ui;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
 import java.util.ArrayList;
 import java.util.List;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 
 public class TabAdapter extends FragmentStatePagerAdapter {
 
