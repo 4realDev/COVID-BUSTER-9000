76659d354e2f7ac9e9954bc6ceb464ae7d73b3a1
Dennis Briner <dennis.briner@lightray.io>
2020-11-29 17:52:53 +0100

    fix parsing error

--- Android/app/src/main/java/com/co2team/covidbuster/service/ThingsSpeakResponseParseService.java
+++ Android/app/src/main/java/com/co2team/covidbuster/service/ThingsSpeakResponseParseService.java
@@ -23,7 +23,11 @@ public class ThingsSpeakResponseParseService {
         JSONArray feeds = (JSONArray) jsonObject.get("feeds");
         for (int i = 0; i < feeds.length(); i++) {
             String createdString = (String) feeds.getJSONObject(i).get("created_at");
-            String co2ppmString = (String) feeds.getJSONObject(i).get("field" + roomId);
+            String co2ppmString = feeds.getJSONObject(i).get("field" + roomId).toString();
+            if(co2ppmString.equals("null")) {
+                // This means we have no value for this field and can ignore this entry
+                continue;
+            }
             int co2ppm = Integer.parseInt(co2ppmString);
 
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
