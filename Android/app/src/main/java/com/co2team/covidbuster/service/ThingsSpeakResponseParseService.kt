7b84eac150fe3fa709e89c3fd7dabe586e9d31a2
Dennis Briner <dennis.briner@lightray.io>
2020-11-30 14:10:08 +0100

    fix date issue and rewrote service to kotlin

--- Android/app/src/main/java/com/co2team/covidbuster/service/ThingsSpeakResponseParseService.kt
+++ Android/app/src/main/java/com/co2team/covidbuster/service/ThingsSpeakResponseParseService.kt
@@ -1,41 +1,38 @@
-package com.co2team.covidbuster.service;
+package com.co2team.covidbuster.service
 
-import com.co2team.covidbuster.model.RoomCo2Data;
+import com.co2team.covidbuster.model.RoomCo2Data
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.util.*
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+class ThingsSpeakResponseParseService {
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
-public class ThingsSpeakResponseParseService {
+    @Throws(JSONException::class)
+    fun parseJsonResponse(jsonResponse: String, roomId: Int): List<RoomCo2Data> {
+        val roomData: MutableList<RoomCo2Data> = ArrayList()
+        val jsonObject = JSONObject(jsonResponse)
 
-    private static final String TAG = BackendService.class.getSimpleName();
-
-    public List<RoomCo2Data> parseJsonResponse(String jsonResponse, int roomId) throws JSONException {
-        List<RoomCo2Data> roomData = new ArrayList<>();
-
-        JSONObject jsonObject = new JSONObject(jsonResponse);
-
-        JSONArray feeds = (JSONArray) jsonObject.get("feeds");
-        for (int i = 0; i < feeds.length(); i++) {
-            String createdString = (String) feeds.getJSONObject(i).get("created_at");
-            String co2ppmString = feeds.getJSONObject(i).get("field" + roomId).toString();
-            if(co2ppmString.equals("null")) {
+        val feeds = jsonObject["feeds"] as JSONArray
+        for (i in 0 until feeds.length()) {
+            val createdString = feeds.getJSONObject(i)["created_at"] as String
+            val co2ppmString = feeds.getJSONObject(i)["field$roomId"].toString()
+            if (co2ppmString == "null") {
                 // This means we have no value for this field and can ignore this entry
-                continue;
+                continue
             }
-            int co2ppm = Integer.parseInt(co2ppmString);
-
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
-            LocalDateTime createdDate = LocalDateTime.parse(createdString, formatter);
-            roomData.add(new RoomCo2Data(co2ppm, createdDate));
-
+            val co2ppm = co2ppmString.toInt()
+            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX")
+            val createdDate = LocalDateTime.parse(createdString, formatter)
+            val createdDateLocalTime = createdDate.atZone(ZoneOffset.UTC).withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime()
+            roomData.add(RoomCo2Data(co2ppm, createdDateLocalTime))
         }
-
-        return roomData;
+        return roomData
     }
+
 }
